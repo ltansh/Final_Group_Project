448763724eae76e30451fd5581ee60ad578a9925
ltansh <130187954+ltansh@users.noreply.github.com>
2023-09-28 14:36:46 -0700

    Added button interactions and scroll reveal js

---
+++ js/index.js
@@ -0,0 +1,53 @@
+ScrollReveal({ reset: true }).reveal(".card:nth-child(1)", {
+    delay: 300,
+  });
+  ScrollReveal({ reset: true }).reveal(".card:nth-child(2)", {
+    delay: 550,
+  });
+  ScrollReveal({ reset: true }).reveal(".card:nth-child(3)", {
+    delay: 800,
+  });
+
+  // Download App Scroll Reveal
+  ScrollReveal({ reset: true }).reveal(".quoteText, .appStore", {
+    delay: 300,
+  });
+  ScrollReveal({ reset: true }).reveal(".appIconCtn", {
+    delay: 550,
+  });
+
+  // How it works Scroll Reveal
+  ScrollReveal({ reset: true }).reveal(".appScreen:nth-child(1)", {
+    delay: 300,
+  });
+  ScrollReveal({ reset: true }).reveal(".appScreen:nth-child(2)", {
+    delay: 550,
+  });
+  ScrollReveal({ reset: true }).reveal(".appScreen:nth-child(3)", {
+    delay: 800,
+  });
+
+  // Influencers Scroll Reveal
+  ScrollReveal({ reset: true }).reveal(".infCard:nth-child(1)", {
+    delay: 300,
+  });
+  ScrollReveal({ reset: true }).reveal(".infCard:nth-child(2)", {
+    delay: 550,
+  });
+  ScrollReveal({ reset: true }).reveal(".infCard:nth-child(3)", {
+    delay: 800,
+  });
+
+  // Events Scroll Reveal
+  ScrollReveal({ reset: true }).reveal("#eventCard1", {
+    delay: 300,
+  });
+  ScrollReveal({ reset: true }).reveal("#eventCard2", {
+    delay: 550,
+  });
+  ScrollReveal({ reset: true }).reveal("#eventCard3", {
+    delay: 800,
+  });
+  ScrollReveal({ reset: true }).reveal("#eventCard4", {
+    delay: 1050,
+  });
